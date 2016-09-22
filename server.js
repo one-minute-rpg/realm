f01eaeaf0df420e6318fef0f653b743e7c8caec7
Pablo Brodt <pablobrodt@gmail.com>
2016-09-21 21:38:05 -0300

    feat(server): adicionar arquivo de inicialização do servidor

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var app = require('./config/express')();
+
+//require('./config/database.js')('mongodb://localhost:27017/contatooh');
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server escutando a porta ' + app.get('port'));
+});
