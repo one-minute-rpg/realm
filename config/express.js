a8903b5985c2ae7160ca5dcc1581e6e8cff298d8
Pablo Brodt <pablobrodt@gmail.com>
2016-09-21 21:38:26 -0300

    feat(express): adicionar configurações basicas do express

---
+++ config/express.js
@@ -0,0 +1,25 @@
+//config/express.js
+var express = require('express');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+
+module.exports = function(){
+    var app = express();
+
+    app.set('port', 3000);
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    load('models', {cwd: 'app'})
+        .then('controllers')
+        .then('routes')
+        .into(app);
+
+    // se nenhum rota atender, direciona para página 404
+    app.get('*', function(req, res) {
+        res.status(404).render('404');
+    });
+
+    return app;
+};
