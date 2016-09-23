49e0da8bfc2e570ec83f92451d48678f94f5878b
Pablo Brodt <pablobrodt@gmail.com>
2016-09-23 00:23:50 -0300

    fix(models): ajuste no schema da model

--- app/models/Story.js
+++ app/models/Story.js
@@ -57,18 +57,132 @@ module.exports = function() {
                     }
                 }
             },
-            itens: {
-                type: [mongoose.Schema.ObjectId],
-                ref: 'Scene'
-            },
-            creatures: {
-                type: [mongoose.Schema.ObjectId],
-                ref: 'Creature'
-            },
-            scenes: {
-                type: [mongoose.Schema.ObjectId],
-                ref: 'Scene'
-            }
+            itens: [{
+                type: {
+                    type: String,
+                    required: true
+                },
+                name: {
+                    type: String,
+                    required: true
+                },
+                text: {
+                    type: String
+                },
+                events: [{
+                    event_type: {
+                        type: String,
+                        required: true
+                    },
+                    text: {
+                        pt_BR: {
+                            type: String
+                        },
+                        en_US: {
+                            type: String
+                        }
+                    },
+                    attribute: {
+                        type: String
+                    },
+                    value: {
+                        type: Number
+                    },
+                    item_id: {
+                        type: mongoose.Schema.ObjectId
+                    },
+                    quantity: {
+                        type: Number
+                    }
+                }]
+            }],
+            creatures: [{
+                name:{
+                    type: String,
+                    required: true
+                },
+                level: {
+                    type: Number,
+                    required: true
+                },
+                attributes: {
+                    health: {
+                        type: Number,
+                        required: true
+                    },
+                    strength: {
+                        type: Number,
+                        required: true
+                    },
+                    agility: {
+                        type: Number,
+                        required: true
+                    },
+                    intelligence: {
+                        type: Number,
+                        required: true
+                    }
+                }
+            }],
+            scenes: [{
+                    type: {
+                        type: String,
+                        required: true
+                    },
+                    title:{
+                        type: String,
+                        required: true
+                    },
+                    text: {
+                        type: String,
+                        required: true
+                    },
+                    actions: [{
+                        text: {
+                            type: String,
+                            required: true
+                        },
+                        require_item_ids: [{
+                            type: [mongoose.Schema.ObjectId], //array contento os ids dos itens
+                        }],
+                        require_attribute_value: {
+                            health: {
+                                type: Number
+                            },
+                            strength: {
+                                type: Number
+                            },
+                            agility: {
+                                type: Number
+                            },
+                            intelligence: {
+                                type: Number
+                            }
+                        },
+                        events: [{
+                            event_type: {
+                                type: String,
+                                required: true
+                            },
+                            text: {
+                                pt_BR: {
+                                    type: String
+                                },
+                                en_US: {
+                                    type: String
+                                }
+                            },
+                            scene_id: {
+                                type: mongoose.Schema.ObjectId
+                            }
+                        }]
+                    }],
+                    challenge:{
+                        type: mongoose.Schema.ObjectId
+                    },
+                    on_win: {},
+                    on_lose: {}
+                }]
         });
 
     return mongoose.model('Story', schema);
