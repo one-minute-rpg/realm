51f4f32ce582de672cc9977c313457719c34be20
Pablo Brodt <pablobrodt@gmail.com>
2016-09-24 11:18:47 -0300

    feat(Route) Adicionar algumas rotas para consulta de historias

---
+++ app/routes/StoryRoute.js
@@ -0,0 +1,15 @@
+// /app/routes/StoryRoute
+// Configurações de rotas para operações com historias
+
+
+module.exports = function(app){
+    var controller = app.controllers.StoryController;
+
+    app.route('/stories')
+        .get(controller.findAll)
+        .post(controller.insert);
+    
+    app.route('/story/:id')
+        .get(controller.findById)
+        .delete(controller.delete);
+}
