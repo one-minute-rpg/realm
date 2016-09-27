51934bb4bec853258a924450c9379230d11e4aad
Pablo Brodt <pablobrodt@gmail.com>
2016-09-26 23:15:21 -0300

    feat(Story): Adicionar model de resumo de historia

---
+++ app/models/StoryResume.js
@@ -0,0 +1,27 @@
+// app/models/StoryResume.js
+// Model para resumo de historia, armazenando apenas dados para pesquisa rapida
+
+var mongoose = require('mongoose');
+
+module.exports = function() {
+    var schema = mongoose.Schema({
+            title:{
+                pt_BR: {
+                    type: String,
+                    required: true
+                },
+                en_US: {
+                    type: String
+                }
+            },
+            story_id:{
+                type: mongoose.Schema.ObjectId,
+                ref: 'Story'
+            },
+            image: {
+                type: String
+            }
+        });
+
+    return mongoose.model('StoryResume', schema);
+};
