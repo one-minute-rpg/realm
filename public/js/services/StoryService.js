b295feac4c59105c18f32b27d8bc707b9ed301d1
Pablo Brodt <pablo.brodt@cwi.com.br>
2016-09-29 17:01:47 -0300

    refactor(StoryService): Refatorar service

--- public/js/services/StoryService.js
+++ public/js/services/StoryService.js
@@ -1,24 +1,21 @@
 angular.module('realm')
     .factory('StoryService', function($resource) {
 
-        var Story = $resource('/myStories');
+        var Story = $resource('/myStories/add');
 
         Story.saveStory = saveStory;
 
         function saveStory(story) {
-
             var resume = createResume(story);
 
+            Story.save();
         }
 
         function createResume(story) {
-
             var resume = {};
 
             resume.title = { pt_BR: story.title.pt_BR };
-
             resume.description = story.description;
-
             resume.cover = story.cover;
 
             return resume;
