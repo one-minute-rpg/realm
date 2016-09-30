9f85d59274323293d05d99fc6d901dd5962d0ab4
Pablo Brodt <pablo.brodt@cwi.com.br>
2016-09-30 14:53:19 -0300

    fix(Model): Corrigir obrigatoriedade d propriedades e adicionadas novas propriedades

--- app/models/Story.js
+++ app/models/Story.js
@@ -5,78 +5,152 @@ var mongoose = require('mongoose');
 
 module.exports = function() {
     var schema = mongoose.Schema({
-            title:{
-                pt_BR: {
-                    type: String,
-                    required: true
-                },
-                en_US: {
-                    type: String
-                }
+        title: {
+            pt_BR: {
+                type: String,
+                required: true
             },
-            character: {
-                itens: {
-                    type: [mongoose.Schema.ObjectID] //Array com ids de itens
+            en_US: {
+                type: String
+            }
+        },
+        description: {
+            type: String
+        },
+        cover: {
+            type: String
+        },
+        character: {
+            itens: {
+                type: [mongoose.Schema.ObjectID] //Array com ids de itens
+            },
+            attributes: {
+                health: {
+                    current: {
+                        type: Number
+                    },
+                    max: {
+                        type: Number
+                    }
                 },
-                attributes: {
-                    health: {
-                        current: {
-                            type: Number,
-                            required: true
-                        },
-                        max: {
-                            type: Number,
-                            required: true
-                        }
+                strength: {
+                    current: {
+                        type: Number
                     },
-                    strength: {
-                        current: {
-                            type: Number,
-                            required: true
-                        },
-                        max: {
-                            type: Number,
-                            required: true
-                        }
+                    max: {
+                        type: Number
+                    }
+                },
+                agility: {
+                    current: {
+                        type: Number
                     },
-                    agility: {
-                        current: {
-                            type: Number,
-                            required: true
-                        },
-                        max: {
-                            type: Number,
-                            required: true
-                        }
+                    max: {
+                        type: Number
+                    }
+                },
+                intelligence: {
+                    current: {
+                        type: Number
                     },
-                    intelligence: {
-                        current: {
-                            type: Number,
-                            required: true
-                        },
-                        max: {
-                            type: Number,
-                            required: true
-                        }
+                    max: {
+                        type: Number
                     }
                 }
+            }
+        },
+        itens: [{
+            type: {
+                type: String
             },
-            itens: [{
-                type: {
-                    type: String,
-                    required: true
+            name: {
+                type: String
+            },
+            text: {
+                type: String
+            },
+            events: [{
+                event_type: {
+                    type: String
                 },
-                name: {
-                    type: String,
-                    required: true
+                text: {
+                    pt_BR: {
+                        type: String
+                    },
+                    en_US: {
+                        type: String
+                    }
                 },
+                attribute: {
+                    type: String
+                },
+                value: {
+                    type: Number
+                },
+                item_id: {
+                    type: mongoose.Schema.ObjectId //Id do item afetado pelo evento
+                },
+                quantity: {
+                    type: Number //Numer oque afetará a quantidade do item
+                }
+            }]
+        }],
+        creatures: [{
+            name: {
+                type: String
+            },
+            level: {
+                type: Number
+            },
+            attributes: {
+                health: {
+                    type: Number
+                },
+                strength: {
+                    type: Number
+                },
+                agility: {
+                    type: Number
+                },
+                intelligence: {
+                    type: Number
+                }
+            }
+        }],
+        scenes: [{
+            type: {
+                type: String
+            },
+            title: {
+                type: String
+            },
+            text: {
+                type: String
+            },
+            actions: [{
                 text: {
                     type: String
                 },
+                require_item_ids: [{
+                    type: [mongoose.Schema.ObjectId], //Array contento os ids dos itens
+                }],
+                require_attribute_value: {
+                    health: {
+                        type: Number
+                    },
+                    strength: {
+                        type: Number
+                    },
+                    agility: {
+                        type: Number
+                    },
+                    intelligence: {
+                        type: Number
+                    }
+                },
                 events: [{
                     event_type: {
-                        type: String,
-                        required: true
+                        type: String
                     },
                     text: {
                         pt_BR: {
@@ -97,113 +171,23 @@ module.exports = function() {
                     },
                     quantity: {
                         type: Number //Numer oque afetará a quantidade do item
+                    },
+                    scene_id: {
+                        type: mongoose.Schema.ObjectId //Id da cena a ser encaminhado
                     }
                 }]
             }],
-            creatures: [{
-                name:{
-                    type: String,
-                    required: true
-                },
-                level: {
-                    type: Number,
-                    required: true
-                },
-                attributes: {
-                    health: {
-                        type: Number,
-                        required: true
-                    },
-                    strength: {
-                        type: Number,
-                        required: true
-                    },
-                    agility: {
-                        type: Number,
-                        required: true
-                    },
-                    intelligence: {
-                        type: Number,
-                        required: true
-                    }
-                }
-            }],
-            scenes: [{
-                    type: {
-                        type: String,
-                        required: true
-                    },
-                    title:{
-                        type: String,
-                        required: true
-                    },
-                    text: {
-                        type: String,
-                        required: true
-                    },
-                    actions: [{
-                        text: {
-                            type: String,
-                            required: true
-                        },
-                        require_item_ids: [{
-                            type: [mongoose.Schema.ObjectId], //Array contento os ids dos itens
-                        }],
-                        require_attribute_value: {
-                            health: {
-                                type: Number
-                            },
-                            strength: {
-                                type: Number
-                            },
-                            agility: {
-                                type: Number
-                            },
-                            intelligence: {
-                                type: Number
-                            }
-                        },
-                        events: [{
-                            event_type: {
-                                type: String,
-                                required: true
-                            },
-                            text: {
-                                pt_BR: {
-                                    type: String
-                                },
-                                en_US: {
-                                    type: String
-                                }
-                            },
-                            attribute: {
-                                type: String
-                            },
-                            value: {
-                                type: Number
-                            },
-                            item_id: {
-                                type: mongoose.Schema.ObjectId //Id do item afetado pelo evento
-                            },
-                            quantity: {
-                                type: Number //Numer oque afetará a quantidade do item
-                            },
-                            scene_id: {
-                                type: mongoose.Schema.ObjectId //Id da cena a ser encaminhado
-                            }
-                        }]
-                    }],
-                    challenge:{
-                        type: mongoose.Schema.ObjectId //Id da criatura do desafio
-                    },
-                    on_win: {
-                        type: mongoose.Schema.ObjectId //Id da cena a ser encaminhado após vencer o desafio
-                    },
-                    on_lose: {
-                        type: mongoose.Schema.ObjectId //Id da cena de game over
-                    }
-                }]
-        });
+            challenge: {
+                type: mongoose.Schema.ObjectId //Id da criatura do desafio
+            },
+            on_win: {
+                type: mongoose.Schema.ObjectId //Id da cena a ser encaminhado após vencer o desafio
+            },
+            on_lose: {
+                type: mongoose.Schema.ObjectId //Id da cena de game over
+            }
+        }]
+    });
 
     return mongoose.model('Story', schema);
 };
