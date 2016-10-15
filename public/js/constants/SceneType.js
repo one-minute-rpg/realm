21ca3bccca5c7a245a2a071d073f38356bdd9d21
Pablo Brodt <pablobrodt@gmail.com>
2016-10-15 20:28:44 -0300

    feat(Constant): Adicionar constante de tipo de cena

---
+++ public/js/constants/SceneType.js
@@ -0,0 +1,7 @@
+angular.module('realm')
+    .constant('SceneType', {
+        DECISION: 'DECISION_SCENE',
+        CHALLENGE :'CHALLENGE_SCENE',
+        FINISH: 'FINISH_SCENE',
+        GAME_OVER: 'GAME_OVER_SCENE'
+    });
