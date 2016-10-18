4cd3afc532be6a3106f56122e522f6e9094355fd
Pablo Brodt <pablobrodt@gmail.com>
2016-10-18 16:09:44 -0200

    fix(Api Route): Remover rota conflitante

--- app/routes/StoryRoute.js
+++ app/routes/StoryRoute.js
@@ -4,9 +4,6 @@ module.exports = function(app) {
     app.route('/myStories/add')
         .post(controller.save);
 
-    app.route('/myStories')
-        .get(controller.find);
-
     app.route('/myStories/:id')
         .get(controller.find)
         .delete(controller.delete);
