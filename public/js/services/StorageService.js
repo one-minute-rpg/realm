1e2583cb2c9296c449a84454412ca866c747fb55
Pablo Brodt <pablobrodt@gmail.com>
2016-11-13 01:49:49 -0200

    refactor(StorageService): Remover métodos não utilizados

--- public/js/services/StorageService.js
+++ public/js/services/StorageService.js
@@ -6,18 +6,8 @@ StorageService.$inject = ['$http'];
 function StorageService($http){
     var service = {};
 
-    var currentStory = {};
-
     service.insert = insert;
     service.update = update;
-
-    service.setCurrentStory = setCurrentStory;
-
-    service.getCurrentStory = getCurrentStory;
-
-    service.getCurrentStoryScenes = getCurrentStoryScenes;
-
-    service.getCurrentStoryItems = getCurrentStoryItems;
 
     function insert(story){
         return $http.post('/myStories/add', story)
@@ -25,7 +15,7 @@ function StorageService($http){
                 setCurrentStory(response.data);
                 return response.data;
             });
-    }
+    };
 
     function update(story){
         if(!story){
@@ -40,26 +30,6 @@ function StorageService($http){
                     console.log(error);
                     alert('Oorreu um erro em StorageService.update');
                 });
-    }
-
-    function setCurrentStory(story){
-        currentStory = story;
-        alertUpdatedCurrentStory();
-    }
-
-    function getCurrentStory(){
-        return currentStory;
-    }
-
-    function getCurrentStoryScenes(){
-        return currentStory.scenes;
-    }
-
-    function getCurrentStoryItems(){
-        debugger;
-        return currentStory.items;
-    }
-
     };
 
     return service;
