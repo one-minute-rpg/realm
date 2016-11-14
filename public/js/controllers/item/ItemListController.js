339ae1ec1681128fa8da8e74e6566e728585b171
Pablo Brodt <pablobrodt@gmail.com>
2016-11-14 17:48:27 -0200

    feat(RemoveItem): Adicionar exclusão de item

--- public/js/controllers/item/ItemListController.js
+++ public/js/controllers/item/ItemListController.js
@@ -1,21 +1,50 @@
 angular.module('realm')
     .controller('ItemListController', ItemListController);
 
-ItemListController.$inject = ['$scope', '$state'];
+ItemListController.$inject = ['$scope', '$state', '$uibModal'];
 
-function ItemListController($scope, $state) {
+function ItemListController($scope, $state, $uibModal) {
     
     $scope.currentPage = 1;
     $scope.itemsPerPage = 5;
     $scope.totalItems = !!$scope.items ? $scope.items.length : 1;
     
     $scope.edit = edit;
+    $scope.remove = remove;
     $scope.setPage = setPage;
 
     function edit(item_id){
         $state.go('editQuestItem', { 
             item_id: item_id,
             story_id: $state.params.story_id
+        });
+    };
+
+    function remove(item){
+        var modal = $uibModal.open({
+                        templateUrl: 'js/directives/components/modal/modal-confirmacao.template.html',
+                        controller: ['$scope', '$uibModalInstance', 'item', function($scope, $uibModalInstance, item){
+                            $scope.item = item;
+                            
+                            $scope.confirm = function(item){
+                                $uibModalInstance.close(item);
+                            };
+
+                            $scope.cancel = function(){
+                                $uibModalInstance.close();
+                            };
+                        }],
+                        resolve: {
+                            item: function(){
+                                return item;
+                            }
+                        }
+                    });
+
+        modal.result.then(function(item){
+            if(!!item){
+                removeItemFromStory(item);
+            };
         });
     };
 
@@ -33,6 +62,14 @@ function ItemListController($scope, $state) {
 
     function refreshList(){
         $scope.itemsForList = filterItemsForList();
+    };
+
+    function removeItemFromStory(item){
+        var index = $scope.items.indexOf(item);
+
+        $scope.items.splice(index, 1);
+
+        refreshList();
     };
 
     refreshList();
