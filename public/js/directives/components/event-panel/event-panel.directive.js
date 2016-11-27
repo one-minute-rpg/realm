40915cd12fc3b99f239d18c60a0c600ee43a08f5
Pablo Brodt <pablobrodt@gmail.com>
2016-11-27 20:55:30 -0200

    feat(Event): Adicionar alteração de eventos, exceto para eventos de ir a cena

--- public/js/directives/components/event-panel/event-panel.directive.js
+++ public/js/directives/components/event-panel/event-panel.directive.js
@@ -17,8 +17,15 @@ angular.module('realm')
     .controller('EventPanelController', ['$scope', '$uibModal', 'EventType', 'Attributes', function($scope, $uibModal, EventType, Attributes){
 
         $scope.addEvent = addEvent;
+        $scope.editEvent = editEvent;
         $scope.remove = remove;
         $scope.convertToEventList = convertToEventList;
+
+        function findEvent(event_id){
+            return $scope.events.find(function(elem){
+                return elem.event_id == event_id;
+            });
+        };
 
         function addEvent(){
             $scope.eventTypeOptions = convertEventTypeToOption();
@@ -37,7 +44,10 @@ angular.module('realm')
             });
 
             modal.result.then(function(event){
-                $scope.events.push(event);
+                debugger;
+                if(!!event)
+                    $scope.events.push(event);
+                
                 $scope.eventList = $scope.convertToEventList($scope.events);
             });
         };
@@ -70,6 +80,42 @@ angular.module('realm')
             });
         };
 
+        function editEvent(event_id){
+            debugger;
+            var event = findEvent(event_id);
+
+            $scope.eventTypeOptions = convertEventTypeToOption();
+            $scope.attributes = convertAttributesToOption();
+
+            var modal = $uibModal.open({
+                templateUrl: 'js/directives/components/event-panel/modal-event-add.template.html',
+                controller: 'ModalEventPanelController',
+                resolve:{
+                    originalEvent: function(){ return event; },
+                    options: function(){ return $scope.eventTypeOptions; },
+                    attributes: function(){ return $scope.attributes; },
+                    availableScenes: function(){ return $scope.availableScenes; },
+                    availableItems: function(){ return $scope.availableItems.slice(); }
+                },
+                link: function($scope, originalEvent){
+                    $scope.originalEvent = originalEvent;
+                },
+                size: 'sm'
+            });
+
+            modal.result.then(function(e){ 
+                debugger;
+                //substituir evento / encontrar forma de distinguir novo de edit
+                var old = findEvent(e.event_id);
+                var index = $scope.events.indexOf(old);
+
+                $scope.events[index] = e;
+                
+
+                $scope.eventList = $scope.convertToEventList($scope.events);
+            });
+        };
+
         function removeEvent(event_id){
             var event = $scope.events.find(function(elem){
                 return elem.event_id == event_id;
@@ -84,6 +130,7 @@ angular.module('realm')
         };
 
         function convertToEventList(events){
+            debugger;
             var eventList = events.map(function(evt){
                 return {
                     event_id: evt.event_id,
@@ -177,21 +224,33 @@ angular.module('realm')
         $scope.availableItems = availableItems;
         $scope.selectedItem = {};
 
+        debugger;
+        $scope.tempEvent = copy($scope.$resolve.originalEvent);
+        refresh();
+
         $scope.refresh = refresh;
+        $scope.confirm = confirm;
         $scope.cancel = cancel;
         $scope.createEvent = createEvent;
+        $scope.updateEvent = updateEvent;
+        $scope.changeItem = changeItem;
+        $scope.changeScene = changeScene;
+
+
+
+        console.log($scope.availableItems);
 
         function refresh(){ 
-            $scope.isChangeAttribute = $scope.eventType == EventType.CHANGE_ATTRIBUTE;
-            $scope.isGoToScene = $scope.eventType == EventType.GO_TO_SCENE;
-            $scope.isAddItem = $scope.eventType == EventType.ADD_ITEM;
-            $scope.isGameOver = $scope.eventType == EventType.GAME_OVER;
+            $scope.isChangeAttribute = $scope.tempEvent.type == EventType.CHANGE_ATTRIBUTE;
+            $scope.isGoToScene = $scope.tempEvent.type == EventType.GO_TO_SCENE;
+            $scope.isAddItem = $scope.tempEvent.type == EventType.ADD_ITEM;
+            $scope.isGameOver = $scope.tempEvent.type == EventType.GAME_OVER;
         };
 
         function createEvent(){
             var event = {};
 
-            switch($scope.eventType){
+            switch($scope.tempEvent.type){
                 case EventType.CHANGE_ATTRIBUTE: event = createChangeAttributeEvent();
                 break;
 
@@ -207,7 +266,37 @@ angular.module('realm')
 
             event.event_id = chance.guid();
 
-             $uibModalInstance.close(event);
+            $uibModalInstance.close(event);
+        };
+
+        function updateEvent(){
+            debugger;
+            var event = {};
+
+            switch($scope.tempEvent.type){
+                case EventType.CHANGE_ATTRIBUTE: event = updateChangeAttributeEvent();
+                break;
+
+                case EventType.GO_TO_SCENE: event = updateGoToSceneEvent();
+                break;
+
+                case EventType.ADD_ITEM: event = updateAddItemEvent();
+                break;
+
+                case EventType.GAME_OVER: event = updateGameOverEvent();
+                break;
+            };
+
+            $uibModalInstance.close(event);
+        };
+
+        function confirm(){
+            if(!!$scope.tempEvent.event_id){
+                debugger;
+                updateEvent();
+            }else{
+                createEvent();
+            };
         };
 
         function cancel(){
@@ -217,25 +306,24 @@ angular.module('realm')
         function createChangeAttributeEvent(){
             var event = {};
 
-            event.type = $scope.eventType;
-            event.attribute = $scope.attribute;
-            event.value = $scope.value;
-            event.text = { pt_br: $scope.attrChangeText };
+            event.type = $scope.tempEvent.type;
+            event.attribute = $scope.tempEvent.attribute;
+            event.value = $scope.tempEvent.value;
+            event.text = $scope.tempEvent.text;;
 
             return event;
         };
 
         function createGameOverEvent(){
             return { 
-                type: EventType.GAME_OVER, 
-                text: { pt_br: $scope.gameover } 
+                type: $scope.tempEvent.type, 
+                text: $scope.tempEvent.text 
             };
         };
 
         function createGoToSceneEvent(){
-            debugger;
             return { 
-                type: $scope.eventType, 
+                type: $scope.tempEvent.type, 
                 scene_id: $scope.selectedScene.scene_id 
             };
         };
@@ -243,9 +331,102 @@ angular.module('realm')
         function createAddItemEvent(){
             debugger;
             return { 
-                type: $scope.eventType, 
+                type: $scope.tempEvent.type, 
                 item_id: $scope.selectedItem.item_id, 
-                quantity: $scope.quantity 
+                quantity: $scope.tempEvent.quantity 
             };
+        };
+
+        function changeItem(selectedItem){
+            $scope.selectedItem = selectedItem;
+        };
+
+        function changeScene(selectedScene){
+            $scope.selectedScene = selectedScene;
+        };
+
+        function copy(originalEvent){
+            debugger;
+            var tmp = {};
+
+            if(!!originalEvent){
+                tmp.event_id = originalEvent.event_id;
+                tmp.type = originalEvent.type;
+                tmp.attribute = originalEvent.attribute;
+                tmp.value = originalEvent.value;
+                tmp.quantity = originalEvent.quantity;
+                tmp.text = originalEvent.text;
+
+                debugger;
+                $scope.selectedItem = findItem(originalEvent.item_id);
+                $scope.selectedScene.scene_id = findScene(originalEvent.scene_id);
+            }else{
+                tmp.type = '';
+                tmp.attribute = '';
+                tmp.value = null;
+                tmp.quantity = null;
+                tmp.text = { pt_br: '' };
+            };
+
+            return tmp;
+        };
+
+        function updateChangeAttributeEvent(){
+            var tempEvent = $scope.tempEvent;
+            
+            return {
+                event_id: tempEvent.event_id,
+                type: tempEvent.type,
+                attribute: tempEvent.attribute,
+                value: tempEvent.value,
+                text: tempEvent.text
+            };
+        };
+
+        function updateGameOverEvent(){
+            var tempEvent = $scope.tempEvent;
+
+            return {
+                event_id: tempEvent.event_id,
+                type: tempEvent.type,
+                text: tempEvent.text
+            };
+        };
+
+        function updateGoToSceneEvent(){
+            var tempEvent = $scope.tempEvent;
+
+            return{
+                event_id: tempEvent.event_id,
+                type: tempEvent.type,
+                scene_id: tempEvent.scene_id
+            };
+        };
+
+        function updateAddItemEvent(){
+            var tempEvent = $scope.tempEvent;
+
+            return{
+                event_id: tempEvent.event_id,
+                type: tempEvent.type,
+                item_id: $scope.selectedItem.item_id,
+                quantity: tempEvent.quantity
+            };
+        };
+
+        function findItem(item_id){
+            var item = $scope.availableItems.find(function(elem){
+                return elem.item_id = item_id;
+            });
+
+            var index = $scope.availableItems.indexOf(item);
+
+            return $scope.availableItems[index];
+        };
+
+        function findScene(scene_id){
+            return $scope.availableScenes.find(function(elem){
+                return elem.scene_id = scene_id;
+            });
         };
     }]);
