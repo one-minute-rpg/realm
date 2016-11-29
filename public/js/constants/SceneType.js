bffdb06b5a3979625d3d11f944cafc1dd778c018
Pablo Brodt <pablobrodt@gmail.com>
2016-11-29 16:44:54 -0200

    refactor(SceneType): Ajustar constante de tipos de cena

--- public/js/constants/SceneType.js
+++ public/js/constants/SceneType.js
@@ -1,7 +1,4 @@
 angular.module('realm')
     .constant('SceneType', {
-        DECISION: 'DECISION_SCENE',
-        CHALLENGE :'CHALLENGE_SCENE',
-        FINISH: 'FINISH_SCENE',
-        GAME_OVER: 'GAME_OVER_SCENE'
+        DECISION: 'DECISION'
     });
